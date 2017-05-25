6b6e74d784a298f5f79c3a4d7b1a1b641e4f6a10
Sharif Ahmad <sharifahmad2061@gmail.com>
2017-05-25 18:53:16 +0500

    correct timeouts issue with check_if_alive and other minor changes

--- DVR.py
+++ DVR.py
@@ -13,8 +13,6 @@ PRINT_LOCK = Lock()
 # each neighbor is stored as a list with router_id,cost and port
 INITIAL_CONFIG_FILE = str("")
 READ_CONFIG_COMP = False
-NO_LOOPBACK_INDEX = 1
-HAS_COME_BACK = False
 
 
 #       structure of distance vector
@@ -61,9 +59,6 @@ def identify_remote_router(remote_address):
 
 def distance_of_x_to_y(start_node, end_node):
     """this function finds the min cost"""
-    # we need to do something of the exception
-    # that will be raised because the base
-    # router_id is not present in n_d_vec
 
     # adding the base router's d_vec to n_d_vec
     # will simplify the problem and we can then
@@ -93,7 +88,8 @@ def distance_of_x_to_y(start_node, end_node):
         # started and hence we can avoid
         # going back
 
-
+        # we may do some memoization here
+        # and hence don't do reevaluation every time
         return min(
             [
                 distance_of_x_to_y(start_node, neighbor) +
@@ -108,7 +104,10 @@ def bellman_ford(router_id, distance_vector):
     bellman ford algorithm is run on the recieved
     distance vector forwarding table is populated
     """
+    # debug info
     global DATA
+    with PRINT_LOCK:
+        print("bellman ford is running on receivel from {}".format(router_id))
     # initially add new destinations to
     #  destinations array
     for every_dest in distance_vector:
@@ -125,23 +124,32 @@ def bellman_ford(router_id, distance_vector):
     for every_dest in DATA["destinations"]:
         if every_dest in all_neighbor_ids:
             continue
+        # this dest is already present in distance
+        # vector with some weight then we don't
+        # need to change it to math.inf
+        elif every_dest in [dest[0] for dest in DATA["distance_vec"]]:
+            continue
         else:
             DATA["distance_vec"].append([every_dest, math.inf])
 
-    # now calculate min cost via bellman ford
-    distance_of_x_to_y()
+    # now calculate min cost to each destination
+    # in distance vector via bellman ford
+    for every_dest in DATA["distance_vec"]:
+        every_dest[1] = distance_of_x_to_y(DATA["router_id"], every_dest[0])
 
 
 # reading thread for recieving incoming distance vector
 # and alive messages
-def reading():
-    """this is for reading incoming data and\)
-    alive messages and responding to them"""
+def recving():
+    """
+    this is for reading incoming data and
+    alive messages and responding to them
+    """
     global SOCKET1
     global PRINT_LOCK
     global DATA
     while True:
-        msg, remote = SOCKET1.recvfrom(512)
+        msg, remote = SOCKET1.recvfrom(1024)
         msg = pickle.loads(msg)
 
         if msg is "is_alive":
@@ -159,8 +167,6 @@ def reading():
             # hence assign it directly
             DATA["n_d_vec"][remote_router_id] = msg
             bellman_ford(remote_router_id, msg)
-        with PRINT_LOCK:
-            print(msg.decode("utf-8"))
 
 
 def sending():
@@ -195,13 +201,15 @@ def check_if_alive():
     start = current_time()
     msg = pickle.dumps("is_alive")
     while True:
+
         if (current_time() - start) < 10:
             continue
+
         for every_one in DATA["neighbor"]:
-            # every_one is a list with router_id, cost and port_no
-            # .values() makes another list of the given
             remote = ("127.0.0.1", every_one[2])
             # out of the present list
+            # decrease waiting time
+            SOCKET1.settimeout(2)
             SOCKET1.sendto(msg, remote)
             try:
                 recv_msg = pickle.loads(SOCKET1.recvfrom(512)[0])
@@ -209,13 +217,15 @@ def check_if_alive():
                     with PRINT_LOCK:
                         print("{} is alive".format(every_one[0]))
             except (OSError, socket.timeout) as e_ra:
+                # reset waiting time
+                SOCKET1.settimeout(socket.getdefaulttimeout())
                 with PRINT_LOCK:
                     print("{} is dead : {}".format(every_one[0], e_ra))
                 index = DATA["neighbor"].index(every_one)
                 DATA["neighbor"].pop(index)
-                # as soon as the neighbor is removed we need to
-                #  do something either call bellman ford or change
-                #  distance vector manually via a function
+                index = DATA["distance_vec"].index([every_one[0], every_one[1]])
+                DATA["distance_vec"].pop(index)
+                bellman_ford(DATA["router_id"], DATA["distance_vec"])
         # ensuring the time diff is always round about 10
         start = current_time()
 
@@ -230,6 +240,8 @@ def interface_thread(file_name):
     # for initial data by the read_config_file func
     # the initial file is stored as string by read_config_file
     global INITIAL_CONFIG_FILE
+    global READ_CONFIG_COMP
+
     if READ_CONFIG_COMP:
         start = current_time()
         while True:
@@ -241,14 +253,21 @@ def interface_thread(file_name):
                 temp = file1.read()
                 file1.close()
                 if INITIAL_CONFIG_FILE == temp:
-                    time.sleep(5)
+                    start = current_time()
                     continue
                 else:
                     list_1 = INITIAL_CONFIG_FILE.split("\n")
                     list_2 = temp.split("\n")
-                    diff = [(ind, x[1]) for ind, x in enumerate(zip(list_1, list_2))
-                            if x[0] != x[1]]
+                    # first line that's no. of lines is never changed
+                    diff = [
+                        (ind, x[1])
+                        for ind, x in enumerate(zip(list_1, list_2))
+                        if x[0] != x[1]
+                    ]
                     for ind, new_str in diff:
+                        # we use ind - 1 because list is unordered
+                        # and line no. is not included here hence
+                        # index is 1 less
                         DATA["neighbor"].pop(ind - 1)
                         new_en = [x for x in new_str.split(" ")]
                         new_en[1] = float(new_en[1])
@@ -272,14 +291,10 @@ def read_config_file(filename):
         while no_of_entries:
             temp_line = file.readline()
             arguments = temp_line.split(" ")
-            DATA["neighbor"].append([arguments[0], arguments[1], arguments[2]])
+            DATA["neighbor"].append([arguments[0], float(arguments[1]), int(arguments[2])])
             no_of_entries -= 1
-
     # also adding base data to n_d_vec
     DATA["n_d_vec"][DATA["router_id"]] = DATA["neighbor"]
-
-    # end of while
-    # end of with
 
 
 def initial_dvec_and_forw_insert():
@@ -309,19 +324,21 @@ def main():
     DATA["port"] = args.port_no
     DATA["router_id"] = args.router_id
     read_config_file(args.router_config_file)
-    initial_dvec_and_forw_insert()
 
     # after the read_config_file func completes we set a variable
     # saying that neighbor data is ready
     global READ_CONFIG_COMP
     READ_CONFIG_COMP = True
 
+    initial_dvec_and_forw_insert()
+
+
     SOCKET1.bind(("", DATA["port"]))  # converts the port to a listening state
     # print(DATA['neighbor'])
 
     # read thread is listening for incoming messages
-    read_th = Thread(target=reading)
-    read_th.start()
+    recv_th = Thread(target=recving)
+    recv_th.start()
 
     # sending thread sends its distance vector to its direct neighbors
     send_th = Thread(target=sending)
